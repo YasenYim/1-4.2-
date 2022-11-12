f41d635117f7f1fd8b857710e7b0182395a0e9ff
niko <66620705+nikoLua@users.noreply.github.com>
2022-11-12 16:12:50 +0800

    Add files via upload

---
+++ 1-4.2 循环输入数字/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1_4._2_循环输入数字
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                { break; }  // break跳出循环
+                int n;
+                if(!int.TryParse(input,out n))
+                { Console.WriteLine("请重新输入数字："); continue; }  // continue的意思是回到while，进行下一次循环
+                Console.WriteLine(n * 2);
+            }
+            Console.WriteLine("程序结束");
+            Console.ReadKey();
+        }
+    }
+}
